247d96d75cb5db0bab1ad686cfd6eecaa9d6176d
Pedro Gama <32106927+fc47081@users.noreply.github.com>
2018-02-26 19:27:24 +0000

    Create user.java

---
+++ src/user.java
@@ -0,0 +1,33 @@
+
+public class user {
+
+	private String UserName;
+	private String password;
+
+	/**
+	 * Construtor
+	 * @param userName
+	 * @param password
+	 */
+	public user(String userName,String password ) {
+		this.UserName = userName;
+		this.password = password;
+
+	}
+
+	/**
+	 * Get nome do user
+	 * @return UserName 
+	 */
+	public String getUserName() {
+		return UserName;
+	}
+
+	/**
+	 * Get password do user
+	 * @return password
+	 */
+	public String getPassword() {
+		return password;
+	}
+}
